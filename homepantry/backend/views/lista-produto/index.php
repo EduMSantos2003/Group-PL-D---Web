818f66a99d8c1e20bd8bb7d2807919459fb54d39
sardor <2241538@my.IPLeiria.pt>
2025-11-07 00:01:26 +0000

    Alt 07/11/2025

---
+++ homepantry/backend/views/lista-produto/index.php
@@ -0,0 +1,48 @@
+<?php
+
+use common\models\ListaProduto;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var common\models\ListaProdutoSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Lista Produtos';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="lista-produto-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Lista Produto', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'idLista',
+            'idProduto',
+            'quantidade',
+            'precoUnitario',
+            //'subTotal',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, ListaProduto $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
