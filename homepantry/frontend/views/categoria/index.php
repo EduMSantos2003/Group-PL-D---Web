e66b56d565580af04951ede17a391a2c222e7c06
EduMSantos2003 <2241535@my.ipleiria.pt>
2025-12-10 20:46:50 +0000

    10/12
    Imagem do titulo

--- homepantry/frontend/views/categoria/index.php
+++ homepantry/frontend/views/categoria/index.php
@@ -11,29 +11,11 @@ use yii\grid\GridView;
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
 $this->title = 'Categorias';
-//$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;
 $this->registerCssFile('@web/css/produtos.css');
 
 ?>
 <div class="categoria-index">
-
-    <?php $this->beginBlock('hero');?>
-
-    <!-- Page Header Start -->
-    <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container">
-            <h1 class="display-3 mb-3 animated slideInDown">Categorias</h1>
-            <nav aria-label="breadcrumb animated slideInDown">
-                <ol class="breadcrumb mb-0">
-                    <li class="breadcrumb-item"><a class="text-body" href="<?= Url::to(['site/index']) ?>">Home</a></li>
-                    <li class="breadcrumb-item"><a class="text-body" href="<?= Url::to(['']) ?>">Pages</a></li>
-                    <li class="breadcrumb-item"><a class="text-body" href="<?= Url::to(['']) ?>">Categorias</a></li>
-                </ol>
-            </nav>
-        </div>
-    </div>
-    <!-- Page Header End -->
-    <?php $this -> endBlock() ?>
 
     <h1><?= Html::encode($this->title) ?></h1>
 
