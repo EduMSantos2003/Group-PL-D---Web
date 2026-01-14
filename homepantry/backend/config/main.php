e34aebe3ae6678684a3e83f6293e3881bba6d693
AndreLopes98 <2241552@my.ipleiria.pt>
2026-01-14 23:47:47 +0000

    aw

--- homepantry/backend/config/main.php
+++ homepantry/backend/config/main.php
@@ -54,7 +54,7 @@ return [
 
         'urlManager' => [
 
-            'enablePrettyUrl' => true,
+            'enablePrettyUrl' => false,
             'showScriptName' => false,
 
             'rules' => [
