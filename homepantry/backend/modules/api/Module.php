30fa114473fb626fc8872447528cd5bb61c862a7
AndreLopes98 <2241552@my.ipleiria.pt>
2026-01-15 00:53:14 +0000

    API6

--- homepantry/backend/modules/api/Module.php
+++ homepantry/backend/modules/api/Module.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace backend\modules\api;
+use Yii;
+use yii\web\Response;
 
 class Module extends \yii\base\Module
 {
-    public $controllerNamespace = 'backend\modules\api\controllers';
-
     public function init()
     {
         parent::init();
+
+        // LIMPA QUALQUER OUTPUT ANTES DO JSON
+        Yii::$app->response->on(Response::EVENT_BEFORE_SEND, function () {
+            if (ob_get_length()) {
+                ob_clean();
+            }
+        });
     }
 }
